58496c63151aeea1087562df3a20f76a81404cac
BrentWar <142748469+BrentWar@users.noreply.github.com>
2023-09-17 16:17:59 -0400

    Implement calculator logic and display

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function operate(lhs, op, rhs) {
         case "-":
             return subtract(lhs, rhs);
             break;
-        case "*":
+        case "x":
             return multiply(lhs, rhs);
             break;
         case "/":
@@ -32,9 +32,14 @@ function operate(lhs, op, rhs) {
 }
 
 
+
+
 let operator = "";
-let firstNumber;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
+let focus = 0;
+
+let display = document.querySelector(".display");
 
 let buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
@@ -50,5 +55,76 @@ buttons.forEach(button => {
     });
     button.addEventListener("mouseup", function() {
         button.classList.remove("click");
-    })
+    });
+
+    button.addEventListener("click", function() {
+        
+        if (button.textContent === "c") {
+            firstNumber = "";
+            secondNumber = "";
+            focus = 0;
+            operator = "";
+            display.textContent = "";
+
+        }
+        else if (focus === 0) {
+            if (!(button.textContent === " + " || button.textContent === " - " || button.textContent === " x " || button.textContent === " / ")) {
+                if (button.textContent !== " = ") {
+                    firstNumber += button.textContent;
+                    display.textContent += button.textContent;
+                }
+            }
+            else {
+                focus = 1;
+                operator = button.textContent.charAt(1);
+                display.textContent += button.textContent;
+            }
+        }
+        else if (focus === 1) {
+            if (button.textContent === " = " && secondNumber !== "") {
+                if (operator === "/" && secondNumber === "0") {
+                    alert("That's not allowed");
+                    firstNumber = "";
+                    secondNumber = "";
+                    focus = 0;
+                    operator = "";
+                    display.textContent = "";
+
+                }
+                else {
+                    display.textContent = Math.round(operate(firstNumber, operator, secondNumber) * 10000) / 10000;
+                    firstNumber = display.textContent;
+                    secondNumber = "";
+                    operator = "";
+                    focus = 0;
+                }
+            } 
+            else if (secondNumber !== "" && (button.textContent === " + " || button.textContent === " - " || button.textContent === " x " || button.textContent === " / ")) {
+                if (operator === "/" && secondNumber === "0") {
+                    alert("That's not allowed");
+                    firstNumber = "";
+                    secondNumber = "";
+                    focus = 0;
+                    operator = "";
+                    display.textContent = "";
+
+                }
+                else {
+                    display.textContent = Math.round(operate(firstNumber, operator, secondNumber) * 10000) / 10000;
+                    firstNumber = display.textContent;
+                    secondNumber = "";
+                    operator = button.textContent.charAt(1);
+                    display.textContent += button.textContent;
+                    focus = 1;
+                }
+            }
+            else if (!(button.textContent === " = " || button.textContent === " + " || button.textContent === " - " || button.textContent === " x " || button.textContent === " / ")) {
+                secondNumber += button.textContent;
+                display.textContent += button.textContent;
+            }
+        }
+    
+    
+    });
+
 });
